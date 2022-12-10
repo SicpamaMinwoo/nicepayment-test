b1869f88eba57d1aebff2036a599a2821ad931e0
SicpamaMinwoo <minwoo@sicpama.com>
2022-12-11 01:34:04 +0900

    apply samsung pay

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { lazy } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-import { Payment, Result } from './Pages';
+import Payment from './Pages/Payment/Payment';
+import Result from './Pages/Result/Result';
+import SamsungPayment from './Pages/SamsungPayment/SamsungPayment';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Payment />}/>
-        <Route exact path="/result" element={<Result />}/>
-      </Routes>
-    </Router>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Payment />}/>
+          <Route exact path="/samsung-payment" element={<SamsungPayment />}/>
+          <Route exact path="/result" element={<Result />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- src/Pages/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Payment } from './Payment/Payment';
-export { default as Result } from './Result/Result';
